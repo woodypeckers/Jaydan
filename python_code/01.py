afda4e772c5e4e38990c1aaa79a9efb90f171440
Jaydanna <871138649@qq.com>
2017-07-09 13:15:35 +0800

    在jaydan项目下推送文件

---
+++ python_code/01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#encoding:utf-8
+
+def MAGIC_CALC(a,b,c = 10):
+    if c == 10:
+        a = 3
+        b = 8
+        return a+b+c
+    else:
+        a = 7
+        b = 8
+        return a+b-c
+MAGIC_CALC()        
+        
+
+
+
+
+
